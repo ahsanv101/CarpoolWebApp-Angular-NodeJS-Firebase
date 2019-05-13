63403edeeaafb1359a0c5fa0d97702d19fa1fafb
Nisha Shaikh <nishashareefshaikh@gmail.com>
2019-05-13 16:00:42 +0500

    Changed logout navigation

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -49,7 +49,7 @@ export class BookComponent implements OnInit {
   logout() {
     this.authService.doLogout()
       .then((res) => {
-        this.location.back();
+        this.router.navigate(['/login']);
       }, (error) => {
         console.log("Logout error", error);
       });
