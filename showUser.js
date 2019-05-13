fb108b287edda7c446dd9f70ec3fc0d66f4dd949
Nisha Shaikh <nishashareefshaikh@gmail.com>
2019-05-13 09:54:58 +0500

    TODO: connect showusers.js with book.component.ts

---
+++ showUser.js
@@ -0,0 +1,29 @@
+
+// change path to carpool-key.json file in main folder
+// const serviceAccount = require("C:/Users/Nisha/Documents/GitHub/HU_Carpool(latest)/carpool-key.json");
+const serviceAccount = require("C:/Users/Nisha/Documents/GitHub/HU_Carpool(latest)/carpool-key.json");
+var admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://carpool-46b66.firebaseio.com"
+  });
+
+function listAllUsers(nextPageToken) {
+    // List batch of users, 1000 at a time.
+    admin.auth().listUsers(1000, nextPageToken)
+        .then(function(listUsersResult) {
+        listUsersResult.users.forEach(function(userRecord) {
+            console.log('user', userRecord.toJSON());
+        });
+        if (listUsersResult.pageToken) {
+            // List next batch of users.
+            listAllUsers(listUsersResult.pageToken);
+        }
+        })
+        .catch(function(error) {
+        console.log('Error listing users:', error);
+    });
+}
+
+// module.exports = listAllUsers();
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -3,13 +3,15 @@ import { ApiService } from '../api.service';
 import { DataSource } from '@angular/cdk/collections';
 import { Observable } from 'rxjs';
 
+declare function listAllUsers(nextPageToken: any): any;
+
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
 export class ShowUserComponent implements OnInit {
-
+  item: any;
   books: any;
   displayedColumns = ['isbn', 'title', 'author'];
   dataSource = new BookDataSource(this.api);
@@ -17,13 +19,16 @@ export class ShowUserComponent implements OnInit {
   constructor(private api: ApiService) { }
 
   ngOnInit() {
-    this.api.getBooks()
-      .subscribe(res => {
-        console.log(res);
-        this.books = res;
-      }, err => {
-        console.log(err);
-      });
+    listAllUsers(null);
+    // this.item = new listAllUsers(null);
+    // console.log(this.item);
+    // this.api.getBooks()
+    //   .subscribe(res => {
+    //     console.log(res);
+    //     this.books = res;
+    //   }, err => {
+    //     console.log(err);
+    //   });
   }
 
 }
